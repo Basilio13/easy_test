759519971638a7871cad9e0bce0ac48257bd2220
Basilio <46874189+Basilio13@users.noreply.github.com>
2021-09-28 14:31:51 +0300

    Add List

---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>{{title}}</h1>
+<ul>
+    <li v-for="element in elements" v-bind:key="element">
+       {{element}}
+    </li>
+</ul>
+</template>
+
+<script>
+import json from '../../data.json'
+export default {
+    data() {
+        return {
+    title: 'List',
+    elements: json
+    }
+}
+}
+</script>
